f820909e4a83f0d223a4fcae01a2b781664e4631
eburmester <43814774+eburmester@users.noreply.github.com>
2018-12-29 15:35:34 -0500

    Update cli.rb

--- lib/GovSpend/cli.rb
+++ lib/GovSpend/cli.rb
@@ -4,6 +4,38 @@ class GovSpend::CLI
 
 	def call
 		puts "Hi! Welcome to GovSpend."
+		options
+	end
+
+	def options
+		puts "What would you like to do?"
+		puts "I accept: 
+			"1" - Menu 
+			"2" - Agency List 
+			"3" - Fiscal Year Search 
+			"4" - Agency Search
+			"5" - Quit"
+	end
+
+	def menu
+		puts "Menu - brings up this dialogue.
+Agency List - brings up the list of available Agencies
+Fiscal Year Search - will prompt for a Fiscal Year to search for the available Agencies active in that year
+	(Optional) prompt to offer a quarterly search to narrow down options
+Agency Search - will prompt for an Agency name to find and provide details for.
+Quit - will exit this program."
+	end
+
+	def agency_list
+
+	end
+
+	def fiscal_year_search
+
+	end
+
+	def agency_search
+
 	end
 
 end
